c6e95b58c6f2eb4f0ede7de762b070c31be2d8e3
Tarun Sudhams <tarun.sudhams1997@gmail.com>
2019-03-15 03:48:31 +0800

    trying out TCP V1

--- TCPReceiver.py
+++ TCPReceiver.py
@@ -4,3 +4,20 @@ import socket
 # create a socket
 # return a new socket object for communications
 socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# use "" means listening to all adapters in this machine
+
+# only valid for IPv4
+sockfd.bind("", 32341)
+
+print("I_am", socket.gethostname(), "and_I_am_listening_...")
+
+new, who = sockfd.accept()  # return the TCP connection
+
+print("A_connection_with", who, "has_been_established")
+
+message = new.recv(50)
+
+print("\'"+message.decode("ascii")+"\'", "is received from", who)
+new.close()
+sockfd.close()

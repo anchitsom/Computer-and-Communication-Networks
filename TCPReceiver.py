316f7a0f0563441ef1ef1fd9160ab379e6a80948
Tarun Sudhams <tarun.sudhams1997@gmail.com>
2019-03-15 04:08:45 +0800

    try except error fixed

--- TCPReceiver.py
+++ TCPReceiver.py
@@ -1,23 +1,30 @@
 import socket
-
+import sys
 
 # create a socket
 # return a new socket object for communications
+sockfd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # use "" means listening to all adapters in this machine
 
 # only valid for IPv4
-sockfd.bind("", 32341)
 
+sockfd.bind(("", 32341))
 print("I_am", socket.gethostname(), "and_I_am_listening_...")
-
+sockfd.listen(5)
 new, who = sockfd.accept()  # return the TCP connection
 
 print("A_connection_with", who, "has_been_established")
+try:
+    message = new.recv(50)
+except socket.error as err:
+    print("Recv Error:", err)
+if message:
+    print("\'"+message.decode("ascii")+"\'", "is received from", who)
+else:
+    print("Connection is broken")
 
-message = new.recv(50)
 
-print("\'"+message.decode("ascii")+"\'", "is received from", who)
 new.close()
 sockfd.close()
--- TCPSender.py
+++ TCPSender.py
@@ -1,21 +1,27 @@
 import socket
+import sys
 
 sockfd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # "localhost" means the loopback device of this machine,
 
 # which uses the IP addr 127.0.0.1
-
-sockfd.connect("localhost", 3234)
-
+try:
+    sockfd.connect(("localhost", 32341))
+except socket.error as err:
+    print("Connection Error", err)
+    sys.exit(1)
 
 print("The_connection_with", sockfd.getpeername(),
 
       "has_been_established")
 
 # Get input for sending
-
-msg = input("Enter your message --> ")
-
+try:
+    msg = input("Enter your message --> ")
+except:
+    print("Terminated abnormally")
+    sockfd.close()
+    sys.exit(1)
 sockfd.send(msg.encode("ascii"))
 
 sockfd.close()
